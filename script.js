02e377012dba593daa1698faa659300646b1841c
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-09-10 15:47:08 -0300

    removendo a animação no css e colocando a animação no javascript da moeda quando o usuario clicar em "Jogar"

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ let cara = 0;
 let coroa = 0;
 
 btnJogar.addEventListener("click", ()=> {
-    contador = Math.floor(Math.random() * 2)
+    contador = Math.floor(Math.random() * 2);
     console.log(contador);
+    moeda.style.animation = "none";
+
+    if(contador) {
+        setTimeout(function(){
+            moeda.style.animation = "moeda-girando 2s forwards"
+        }, 100)
+        console.log("cara");
+    }else {
+        setTimeout(function() {
+            moeda.style.animation = "moeda-coroa 2s forwards"
+        })
+    }
 })
