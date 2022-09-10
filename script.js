8f6a8d50fcc521afd85c650b93a5f899e048de25
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-09-10 14:18:16 -0300

    encontrando o botao de jogar e resetar, e a moeda pelo JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+btnJogar = document.querySelector("#btn-jogar");
+btnResetar = document.querySelector("#btn-resetar");
+moeda = document.querySelector(".imagens-moeda");
+let cara = 0;
+let coroa = 0;
+
